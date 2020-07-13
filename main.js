9c1054b6da45a1eaa039b441e36b5d633782e230
miguelCassimiro99 <miguel.cassimiro99@gmail.com>
2020-07-12 21:09:21 -0300

    Created the objects that will be showed on the index.html

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 var pokemon_name = document.querySelector("#pokemon_name");
 var form = document.querySelector("#form");
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var pokemon_search = "https://pokeapi.co/api/v2/pokemon/"+pokemon_name.value;
+    var pokemon_search = "https://pokeapi.co/api/v2/pokemon/" + pokemon_name.value;
 
     axios.get(pokemon_search)
-    .then(function(response){
-    console.log(response.data)
-    })
-    .catch(function(error){
-        console.log(error);
-        alert("Something went wrong X(");
-    });
+        .then(function(response) {
+
+            let ability = response.data.abilities;
+            let forms = response.data.forms;
+            let type = response.data.types;
+            let moves = response.data.moves;
+
+            console.log(forms);
+
+        })
+        .catch(function(error) {
+            console.log(error);
+            alert("Something went wrong X(");
+        });
 });
 
+/* 
+    name
+    types
+    abilities
+    moves
+
+*/
